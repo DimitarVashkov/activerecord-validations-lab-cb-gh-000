b7f2c92abb0204c43c486b953812010c54ee170d
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-10 05:37:26 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250 }
   validates :summary, length: {maximum: 250 }
   validates :category, inclusion: {in: ['Fiction','Non-Fiction']}
+  validates :click_bait
+
+  def click_bait
+    
+  end
 end
